ca081ef8cc243d704afca46b05c9d550daec6aaa
unknown <made0008@student.monash.edu>
2024-02-23 23:37:04 +0700

    add hpc configs

---
+++ projects/configs/execution/cmdha_12pt_nopos3d_4gpu16bs.py
@@ -0,0 +1,7 @@
+_base_ = [
+    "../runtime_settings/4gpu16bs_run.py",
+    "../settings/cmdha_12pt_nopos3d_nocp.py",
+    "../runtime_settings/debug.py"
+]
+
+debug_modules=['Petr3D', 'CircularDeformAttn', 'Projections', 'IQTransformerEncoder']
