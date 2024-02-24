51e0e8ad46692e981981d2c90f08d0a0f993c509
NaomiEX <michelle.adeline.25@gmail.com>
2024-02-24 00:50:25 +0000

    add hpc cfg

---
+++ projects/configs/execution/cmdha_12pt_4ptenc_24ptdec_1gpu2bs.py
@@ -0,0 +1,7 @@
+_base_ = [
+    "../runtime_settings/1gpu2bs_run.py",
+    "../settings/cmdha_12pt_4ptenc_24ptdec_nocp.py",
+    "../runtime_settings/debug.py"
+]
+
+debug_modules=['MDHA', 'CustomDeformAttn', 'Projections', 'IQTransformerEncoder']
