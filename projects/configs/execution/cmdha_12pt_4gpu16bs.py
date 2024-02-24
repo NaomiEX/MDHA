11ba8a40baba8528d7217454cc219e950eb62322
NaomiEX <michelle.adeline.25@gmail.com>
2024-02-24 13:08:25 +0800

    cmdha cfg

--- projects/configs/execution/cmdha_12pt_4gpu16bs.py
+++ projects/configs/execution/cmdha_12pt_4gpu16bs.py
@@ -1,7 +1,7 @@
 _base_ = [
     "../runtime_settings/4gpu16bs_run.py",
-    "../settings/cmdha_12pt_nocp.py",
+    "../settings/nocp/cmdha_12pt_nocp.py",
     "../runtime_settings/debug.py"
 ]
 
-debug_modules=['MDHA', 'CircularDeformAttn', 'Projections', 'IQTransformerEncoder']
+debug_modules=['MDHA', 'CustomDeformAttn', 'Projections', 'IQTransformerEncoder']
