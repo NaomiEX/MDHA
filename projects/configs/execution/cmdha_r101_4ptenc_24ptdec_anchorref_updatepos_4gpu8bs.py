fbefa24a7f8c9950096671672971ef3dcbf76c69
NaomiEX <michelle.adeline.25@gmail.com>
2024-02-27 01:03:31 +0800

    r101 config

--- projects/configs/execution/cmdha_r101_4ptenc_24ptdec_anchorref_updatepos_4gpu8bs.py
+++ projects/configs/execution/cmdha_r101_4ptenc_24ptdec_anchorref_updatepos_4gpu8bs.py
@@ -1,5 +1,5 @@
 _base_ = [
-    "../runtime_settings/4gpu16bs_run.py",
+    "../runtime_settings/4gpu8bs_run_r101.py",
     "../settings/cmdha_r101_4ptenc_24ptdec_anchorref_updatepos.py",
     "../runtime_settings/debug.py"
 ]
