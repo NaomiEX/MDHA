dc350806977d48acb8108d689d16c6d95c706906
NaomiEX <michelle.adeline.25@gmail.com>
2024-02-27 07:22:50 +0000

    24pt conv cfg

---
+++ projects/configs/execution/cmdha_24pt_anchorref_convdepth_mult_updatepos_4gpu16bs.py
@@ -0,0 +1,8 @@
+_base_ = [
+    "../runtime_settings/4gpu16bs_run.py",
+    "../settings/nocp/cmdha_24pt_anchorref_updatepos_convdepth_mult.py",
+    "../runtime_settings/debug.py"
+]
+
+debug_modules=['CustomDeformAttn', 'Projections', 'IQTransformerEncoder',
+               'AnchorRefinement', 'ReferencePoints']
