bec4ddc1d66e3802dca3ab6219c3e97ec8280448
unknown <made0008@student.monash.edu>
2024-03-04 14:06:32 +0700

    improved slurm train script

--- tools/slurm_train.sh
+++ tools/slurm_train.sh
@@ -7,7 +7,7 @@ JOB_NAME=$2
 GPUS=$3
 CONFIG=$4
 WORK_DIR=$5
-GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+GPUS_PER_NODE=${GPUS_PER_NODE:-4}
 CPUS_PER_TASK=${CPUS_PER_TASK:-5}
 MEM=${MEM:-80G}
 SRUN_ARGS=${SRUN_ARGS:-""}
@@ -16,7 +16,7 @@ PY_ARGS=${@:6}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 srun -p ${PARTITION} \
     --job-name=${JOB_NAME} \
-    --gres=gpu:${GPUS} \
+    --gres=gpu:a100:${GPUS_PER_NODE} \
     --nodes=1 \
     --mem=${MEM} \
     --ntasks=${GPUS} \
