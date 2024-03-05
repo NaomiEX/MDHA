286a7017b76d83cd00408022387f59b034d30d01
NaomiEX <michelle.adeline.25@gmail.com>
2024-03-05 11:28:42 +0800

    fix hpc fg

--- projects/configs/execution/hpc/cmdha_r101_4ptenc_24ptdec_anchorref_updatepos_8gpu16bs.py
+++ projects/configs/execution/hpc/cmdha_r101_4ptenc_24ptdec_anchorref_updatepos_8gpu16bs.py
@@ -1,7 +1,7 @@
 _base_ = [
-    "../runtime_settings/8gpu16bs_run_r101.py",
-    "../settings/cmdha_r101_4ptenc_24ptdec_anchorref_updatepos.py",
-    "../runtime_settings/debug.py"
+    "../../runtime_settings/8gpu16bs_run_r101.py",
+    "../../settings/cmdha_r101_4ptenc_24ptdec_anchorref_updatepos.py",
+    "../../runtime_settings/debug.py"
 ]
 
 debug_modules=['CustomDeformAttn', 'Projections', 'IQTransformerEncoder',
