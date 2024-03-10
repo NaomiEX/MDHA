1b52d4e0b269b7f906eb6fcfa28467195285faca
unknown <made0008@student.monash.edu>
2024-03-10 18:52:31 +0700

    resume

---
+++ projects/configs/execution/hpc/cmdha_r101_4ptenc_24ptdec_convdepth_updatepos_v2_8gpu16bs_resume.py
@@ -0,0 +1,9 @@
+_base_ = [
+    "../../runtime_settings/8gpu16bs_run_r101_resume.py",
+    "../../settings/cmdha_r101_4ptenc_24ptdec_convdepth_updatepos_v2.py",
+    "../../runtime_settings/debug.py"
+]
+
+debug_modules=['CustomDeformAttn', 'Projections', 'IQTransformerEncoder',
+               'AnchorRefinement', 'PETRTransformerDecoder', 'DepthNet',
+               'StreamPETRHead']
