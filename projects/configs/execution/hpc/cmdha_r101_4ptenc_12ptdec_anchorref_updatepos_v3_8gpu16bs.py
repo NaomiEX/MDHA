408eb5a70828086b4f0e874f419699def27f8c6d
unknown <made0008@student.monash.edu>
2024-03-11 21:20:47 +0700

    add anchorref v3

---
+++ projects/configs/execution/hpc/cmdha_r101_4ptenc_12ptdec_anchorref_updatepos_v3_8gpu16bs.py
@@ -0,0 +1,8 @@
+_base_ = [
+    "../../runtime_settings/8gpu16bs_run_r101_lr4e-4.py",
+    "../../settings/cmdha_r101_4ptenc_12ptdec_anchorref_updatepos_v3.py",
+    "../../runtime_settings/debug.py"
+]
+
+debug_modules=['CustomDeformAttn', 'Projections', 'IQTransformerEncoder',
+               'AnchorRefinement', 'PETRTransformerDecoder', 'StreamPETRHead']
