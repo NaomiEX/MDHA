02a3ea81cc9369d5206bc57b6df6d0e1115caf8a
NaomiEX <michelle.adeline.25@gmail.com>
2024-03-11 12:52:29 +0800

    add bind cfg

--- projects/configs/execution/local/cmdha_4ptenc_24ptdec_anchorref_convdepth_mult_updatepos_newproj_bind_2gpu2bs.py
+++ projects/configs/execution/local/cmdha_4ptenc_24ptdec_anchorref_convdepth_mult_updatepos_newproj_bind_2gpu2bs.py
@@ -1,6 +1,6 @@
 _base_ = [
     "../../runtime_settings/2gpu2bs_run.py",
-    "../../settings/cmdha_4ptenc_24ptdec_anchorref_updatepos_convdepth_mult_newproj_bind.py",
+    "../../settings/ablation/cmdha_4ptenc_24ptdec_anchorref_updatepos_convdepth_mult_newproj_bind.py",
     "../../runtime_settings/debug.py"
 ]
 
