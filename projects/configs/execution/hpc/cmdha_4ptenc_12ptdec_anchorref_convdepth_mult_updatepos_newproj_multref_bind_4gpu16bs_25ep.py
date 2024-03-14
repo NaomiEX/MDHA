1649b71867a62f16bf7a65421bd6c51c1bfce7c6
unknown <made0008@student.monash.edu>
2024-03-14 15:13:22 +0800

    add mult bind cfg

--- projects/configs/execution/hpc/cmdha_4ptenc_12ptdec_anchorref_convdepth_mult_updatepos_newproj_multref_bind_4gpu16bs_25ep.py
+++ projects/configs/execution/hpc/cmdha_4ptenc_12ptdec_anchorref_convdepth_mult_updatepos_newproj_multref_bind_4gpu16bs_25ep.py
@@ -1,6 +1,6 @@
 _base_ = [
     "../../runtime_settings/4gpu16bs_run_25ep.py",
-    "../../settings/ablation/cmdha_4ptenc_12ptdec_anchorref_updatepos_convdepth_mult_newproj_multref_bind.py",
+    "../../settings/ablation/cmdha_4ptenc_12ptdec_anchorref_updatepos_convdepth_mult_newproj_multref_bind_nocp.py",
     "../../runtime_settings/debug.py"
 ]
 
