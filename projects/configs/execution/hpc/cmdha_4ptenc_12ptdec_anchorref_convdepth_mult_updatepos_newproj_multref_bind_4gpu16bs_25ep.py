60941b756e87ddb0e86a8ef061840a1e20540264
unknown <made0008@student.monash.edu>
2024-03-14 11:21:04 +0800

    add hpc cfg

---
+++ projects/configs/execution/hpc/cmdha_4ptenc_12ptdec_anchorref_convdepth_mult_updatepos_newproj_multref_bind_4gpu16bs_25ep.py
@@ -0,0 +1,8 @@
+_base_ = [
+    "../../runtime_settings/4gpu16bs_run_25ep.py",
+    "../../settings/ablation/cmdha_4ptenc_12ptdec_anchorref_updatepos_convdepth_mult_newproj_multref_bind.py",
+    "../../runtime_settings/debug.py"
+]
+
+debug_modules=['CustomDeformAttn', 'Projections', 'IQTransformerEncoder',
+               'AnchorRefinement', 'ReferencePoints', 'DepthNet', 'PETRTransformerDecoder']
