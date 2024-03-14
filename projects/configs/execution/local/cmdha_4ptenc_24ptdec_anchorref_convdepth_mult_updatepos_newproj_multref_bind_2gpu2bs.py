9a8d52574905eb7c27f42c0f462ef46d7c7e864d
unknown <made0008@student.monash.edu>
2024-03-14 09:28:36 +0800

    mult ref bind cfg

---
+++ projects/configs/execution/local/cmdha_4ptenc_24ptdec_anchorref_convdepth_mult_updatepos_newproj_multref_bind_2gpu2bs.py
@@ -0,0 +1,8 @@
+_base_ = [
+    "../../runtime_settings/2gpu2bs_run.py",
+    "../../settings/ablation/cmdha_4ptenc_12ptdec_anchorref_updatepos_convdepth_mult_newproj_multref_bind.py",
+    "../../runtime_settings/debug.py"
+]
+
+debug_modules=['CustomDeformAttn', 'Projections', 'IQTransformerEncoder',
+               'AnchorRefinement', 'ReferencePoints', 'DepthNet', 'PETRTransformerDecoder']
